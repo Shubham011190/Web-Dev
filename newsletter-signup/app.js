5b1c88312424b6aa7ca7458c98604e3febd8e0a1
Shubham Singhal <shubham.011190@gmail.com>
2020-02-25 08:48:43 +0530

    CSS load error resolved

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 
+
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
+app.use("/css",express.static("css"));
 
 app.get("/", function(req,res){
   res.sendFile( __dirname + "/signup.html" );
