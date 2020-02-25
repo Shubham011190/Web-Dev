853816495b531aba52b5c6d031164498d8c106a0
Shubham Singhal <shubham.011190@gmail.com>
2020-02-25 09:30:57 +0530

    public folder added for static files

--- newsletter-signup/app.js
+++ newsletter-signup/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
+// const request = require('request');
 
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
-app.use("/css",express.static("css"));
+app.use(express.static("public"));
 
 app.get("/", function(req,res){
   res.sendFile( __dirname + "/signup.html" );
