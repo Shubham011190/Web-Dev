5ebf370a2ffc84597df559d68610dc43fdc804a3
Shubham Singhal <shubham.011190@gmail.com>
2020-03-20 04:33:55 +0530

    Mongoose initial

--- fruits_mongo/app.js
+++ fruits_mongo/app.js
@@ -1,58 +1,23 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-// Database Name
-const dbName = 'fruitsdb';
-// Create a new MongoClient
-const client = new MongoClient(url, { useUnifiedTopology: true });
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-  const db = client.db(dbName);
-  // insertDocuments(db,function(){
-  //   client.close();
-  // });
+mongoose.connect("mongodb://localhost:27017/fruitsDB");
 
-  findDocuments(db,function(){
-    client.close();
-  })
+//Creating schema.
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String
 });
 
-const insertDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Insert some documents
-  collection.insertMany([
-    {
-      name: "Orange", score: 9, review: "Awesome"
-    },
-    {
-      name: "Banana", score :8, review:"Good"
-    },
-    {
-      name:"Kiwi", score:6, review: "Could be better"
-    }
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-}
+//Describing collection. Use singular string since it automatically gets converted to plural.
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
+//Adding a document to the collection.
+const fruit = new Fruit({
+  name:"Apple",
+  rating:7,
+  review: "Pretty solid"
+});
 
-const findDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Find some documents
-  collection.find({}).toArray(function(err, fruits) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(fruits)
-    callback(fruits);
-  });
-}
+//Saving the document to the collection
+fruit.save();
