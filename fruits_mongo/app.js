3cef0eb6dd90ad5c7692d56cb5a5133a1b1f599b
Shubham Singhal <shubham.011190@gmail.com>
2020-03-19 17:58:47 +0530

    Read operation added

--- fruits_mongo/app.js
+++ fruits_mongo/app.js
@@ -12,7 +12,11 @@ client.connect(function(err) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
   const db = client.db(dbName);
-  insertDocuments(db,function(){
+  // insertDocuments(db,function(){
+  //   client.close();
+  // });
+
+  findDocuments(db,function(){
     client.close();
   })
 });
@@ -37,5 +41,18 @@ const insertDocuments = function(db, callback) {
     assert.equal(3, result.ops.length);
     console.log("Inserted 3 documents into the collection");
     callback(result);
+  });
+}
+
+
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('fruits');
+  // Find some documents
+  collection.find({}).toArray(function(err, fruits) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(fruits)
+    callback(fruits);
   });
 }
