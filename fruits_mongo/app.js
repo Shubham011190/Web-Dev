56256aa9b5599bd891d3f1de24127024c64e08d6
Shubham Singhal <shubham.011190@gmail.com>
2020-03-19 17:38:59 +0530

    Update

--- fruits_mongo/app.js
+++ fruits_mongo/app.js
@@ -3,19 +3,14 @@ const assert = require('assert');
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
-
 // Database Name
 const dbName = 'myproject';
-
 // Create a new MongoClient
 const client = new MongoClient(url);
-
 // Use connect method to connect to the Server
 client.connect(function(err) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
-
   const db = client.db(dbName);
-
   client.close();
 });
