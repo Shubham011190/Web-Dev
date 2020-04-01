8d836302fc0cc11b288bef5f2ddf61e42ff304a4
Shubham Singhal <shubham.011190@gmail.com>
2020-04-01 14:08:25 +0530

    Schema

--- Wiki-REST/app.js
+++ Wiki-REST/app.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const ejs = require('ejs');
 const express = require('express');
 const bodyparser = require('body-parser');
+const app = express();
 
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(express.static("public"));
@@ -9,12 +10,17 @@ app.set('view engine', 'ejs');
 
 mongoose.connect("mongodb://localhost:27107/wikiDB",{ useNewUrlParser: true, useUnifiedTopology: true  });
 
-var ArticleSchema = new Schema({
+var ArticleSchema = new mongoose.Schema({
   title: String,
   content: String
 });
 
 var Article = mongoose.model("Article",ArticleSchema);
+
+
+app.get("/",function(req,res){
+  res.send("Hello");
+})
 
 app.listen(3000, function(){
   console.log("Server started at port 3000");
