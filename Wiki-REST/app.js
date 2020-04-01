a1204ba78269fa200878248e6733bcaadcc4aa38
Shubham Singhal <shubham.011190@gmail.com>
2020-04-01 21:50:05 +0530

    Update

--- Wiki-REST/app.js
+++ Wiki-REST/app.js
@@ -22,6 +22,12 @@ app.get("/",function(req,res){
   res.send("Hello");
 })
 
+app.get("/articles",function(req,res){
+  Article.find(function(err,foundArticles){
+    console.log(foundArticles);
+  });
+});
+
 app.listen(3000, function(){
   console.log("Server started at port 3000");
 })
