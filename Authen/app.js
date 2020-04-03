fc1aac2e6ae7e4c79961f5af03e25a285b0ef421
Shubham Singhal <shubham.011190@gmail.com>
2020-04-03 21:18:02 +0530

    mongoDB connection

--- Authen/app.js
+++ Authen/app.js
@@ -1,25 +1,27 @@
 const ejs = require('ejs');
 const express = require('express');
 const bodyparser = require('body-parser');
-
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.static("public"));
-app.set('view engine','ejs');
-app.use(bodyparser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.use(bodyparser.urlencoded({ extended: true }));
 
-app.get("/",function(req,res){
-  res.render("home")
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
+
+app.get("/", function(req, res) {
+    res.render("home")
 });
 
-app.get("/login",function(req,res){
-  res.render("login")
+app.get("/login", function(req, res) {
+    res.render("login")
 });
 
-app.get("/register",function(req,res){
-  res.render("register")
+app.get("/register", function(req, res) {
+    res.render("register")
 });
 
-app.listen(3000,function(){
-  console.log("Server started at 3000");
+app.listen(3000, function() {
+    console.log("Server started at 3000");
 })
