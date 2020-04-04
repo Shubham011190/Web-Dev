d9c2ce21c2a775bccd62e6fcbc5264abe9724b7d
Shubham Singhal <shubham.011190@gmail.com>
2020-04-04 21:50:05 +0530

    Login MD5

--- Authen/app.js
+++ Authen/app.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
 // console.log(process.env.SECRET);
 
 // const secret = "Mysecret1."
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });     -->Used with mongoose-encryption
 
 const User = new mongoose.model("User", userSchema);
 
@@ -55,7 +55,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({ email: username }, function(err, foundUser) {
         if (err) {
             res.send(err)
