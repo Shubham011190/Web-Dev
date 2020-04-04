888ff907519a4d304bcc4d4ccaa47d1d57791230
Shubham Singhal <shubham.011190@gmail.com>
2020-04-04 17:23:00 +0530

    MD5 hashing

--- Authen/app.js
+++ Authen/app.js
@@ -4,24 +4,25 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
-const encrypt = require('mongoose-encryption');
+const md5 = require("md5");
+// const encrypt = require('mongoose-encryption');
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyparser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const userSchema = new mongoose.Schema({
-  email : String,
-  password : String
+    email: String,
+    password: String
 });
 // console.log(process.env.SECRET);
 
 // const secret = "Mysecret1."
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields :['password']});
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
-const User = new mongoose.model("User",userSchema);
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res) {
     res.render("home")
@@ -37,35 +38,33 @@ app.get("/register", function(req, res) {
 
 /*--------------------------------------------------------------------------------------------------------------------------------------------------------POST functions--------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */
 
-app.post("/register", function(req,res){
-  const newUser = new User({
-    email : req.body.username,
-    password: req.body.password
-  });
-  newUser.save(function(err){
-    if(err){
-      res.send(err);
-    }
-    else {
-      res.render("secrets");
-    }
-  })
+app.post("/register", function(req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: md5(req.body.password)
+    });
+    newUser.save(function(err) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.render("secrets");
+        }
+    })
 
 })
 
-app.post("/login",function(req,res){
-  const username = req.body.username;
-  const password = req.body.password;
-  User.findOne({email: username},function(err,foundUser){
-    if(err){
-      res.send(err)
-    }
-    else {
-      if(foundUser.password == password){
-        res.render("secrets");
-      }
-    }
-  })
+app.post("/login", function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({ email: username }, function(err, foundUser) {
+        if (err) {
+            res.send(err)
+        } else {
+            if (foundUser.password == password) {
+                res.render("secrets");
+            }
+        }
+    })
 });
 
 
