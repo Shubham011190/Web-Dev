6f9dc07564021be95beb0b04616be6a7e2ce1655
Shubham Singhal <shubham.011190@gmail.com>
2020-04-07 22:03:17 +0530

    Update

--- Authen/app.js
+++ Authen/app.js
@@ -32,7 +32,8 @@ mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
