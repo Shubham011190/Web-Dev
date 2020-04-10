468a90558412bc4f98222cdc7dca1a15facaa3ed
Shubham Singhal <shubham.011190@gmail.com>
2020-04-10 16:49:21 +0530

    Notes displayed

--- Keeper App/src/components/App.js
+++ Keeper App/src/components/App.js
@@ -2,12 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from '../notes'
+
+function createNote(data) {
+  return  <Note
+    //key={data.key}
+    title={data.title}
+    content={data.content}
+  />
+
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNote)}
       <Footer />
     </div>
   );
--- Keeper App/src/components/Note.js
+++ Keeper App/src/components/Note.js
@@ -1,14 +1,14 @@
 import React from "react";
-import notes from '../notes'
+//import notes from '../notes'
 
-function createNote(data) {
-  <Note
-    key={data.key};
-  
 
-}
-
-function Note() {
+function Note(props) {
   return (
     <div className="note">
-      <h1>This is the no
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+    </div>
+  );
+}
+
+export default Note;
