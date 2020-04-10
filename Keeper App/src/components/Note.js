48a00bc57ef0d1494e9ee67fe0a58583cdd3f73a
Shubham Singhal <shubham.011190@gmail.com>
2020-04-10 16:42:55 +0530

    Initial commit

---
+++ Keeper App/src/components/Note.js
@@ -0,0 +1,14 @@
+import React from "react";
+import notes from '../notes'
+
+function createNote(data) {
+  <Note
+    key={data.key};
+  
+
+}
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is the no
