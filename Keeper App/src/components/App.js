bdfe984216b50d5c79f69d17451df0b148e7f2d3
Shubham Singhal <shubham.011190@gmail.com>
2020-04-13 21:01:15 +0530

    Update

--- Keeper App/src/components/App.js
+++ Keeper App/src/components/App.js
@@ -2,22 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from '../notes'
-
-function createNote(data) {
-  return  <Note
-    key={data.key}
-    title={data.title}
-    content={data.content}
-  />
-
-}
+import CreateArea from "./CreateArea";
 
 function App() {
   return (
     <div>
       <Header />
-      {notes.map(createNote)}
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
