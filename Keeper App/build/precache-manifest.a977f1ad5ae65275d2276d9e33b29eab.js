3b0e085e4d2883f0c1bd0eb8c04f5258c9cbfd1d
Shubham Singhal <shubham.011190@gmail.com>
2020-04-14 20:06:19 +0530

    Update

---
+++ Keeper App/build/precache-manifest.a977f1ad5ae65275d2276d9e33b29eab.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8482bc6c8569106df00348dea724aaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d9c4039fa7e9c9ab63e",
+    "url": "/static/js/2.fcf4c2fb.chunk.js"
+  },
+  {
+    "revision": "35dd4f683fd4cae0862f",
+    "url": "/static/js/main.20b17465.chunk.js"
+  },
+  {
+    "revision": "541060714581fea7a8c4",
+    "url": "/static/js/runtime-main.e30c3f94.js"
+  }
+]);
