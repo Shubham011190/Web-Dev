abac177176cb1a575d5d81d554cd43ed07574db7
Shubham Singhal <shubham.011190@gmail.com>
2020-04-15 12:40:12 +0530

    App.js under src

---
+++ MERN_tutorial/client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import EmployeeTable from './components/EmployeeTable';
+import Form from './components/Form';
+import Message from './components/Message';
+import EmployeeAPI from './EmployeeAPI';
+
+class App extends React.Component{
+    constructor(props) {
+        super(props)
+    }
+}
