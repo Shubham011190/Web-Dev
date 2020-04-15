a65c403970765b79c9b61f3a298dbb9d11ec2041
Shubham Singhal <shubham.011190@gmail.com>
2020-04-16 00:19:37 +0530

    Database connected

--- MERN-fcc/mern-exercise-tracker/backend/server.js
+++ MERN-fcc/mern-exercise-tracker/backend/server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require("mongoose");
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const URI = process.env.ATLAS_URI;
+mongoose.connect(URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
 
 app.use(cors());
 app.use(express.json());
 
 app.listen(PORT, () => {
     console.log("Server started at port " + PORT);
-})
+});
