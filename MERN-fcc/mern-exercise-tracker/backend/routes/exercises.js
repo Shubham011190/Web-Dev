5b4ffdd9383d2aaec8bb82652bf2a59eff052b4f
Shubham Singhal <shubham.011190@gmail.com>
2020-04-16 13:19:53 +0530

    Server started

--- MERN-fcc/mern-exercise-tracker/backend/routes/exercises.js
+++ MERN-fcc/mern-exercise-tracker/backend/routes/exercises.js
@@ -1,10 +1,10 @@
-const router = require('express').Router;
+const router = require('express').Router();
 let Exercise = require('../models/exercise.model');
 
 router.route('/').get((req, res) => {
     Exercise.find()
         .then(exercises => res.json(exercises))
-        .catch(err => res.status(400).json("Error:" + er));
+        .catch(err => res.status(400).json("Error:" + err));
 });
 
 router.route('/add').post((req, res) => {
@@ -22,5 +22,13 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json("Err:" + err));
 
 });
+
+router.route('/:id').get((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => res.json(exercise))
+        .catch(err => res.status(400).json("Err = " + err));
+});
+
+ 
 
 module.exports = router;
