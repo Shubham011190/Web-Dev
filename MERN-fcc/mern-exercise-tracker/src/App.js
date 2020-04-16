f6cb3607b6d8220e7f0368b430a8cb535314a1e0
Shubham Singhal <shubham.011190@gmail.com>
2020-04-16 13:56:21 +0530

    Update

--- MERN-fcc/mern-exercise-tracker/src/App.js
+++ MERN-fcc/mern-exercise-tracker/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import {BrowserRouter as Router,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Navbar from './components/navbar.component.js'
+import ExercisesList from './components/exercises-list.component'
+import EditExercise from './components/edit-exercise.component'
+import CreateExercise from './components/create-exercise.component'
+import CreateUser from './components/create-user.component'
 
 function App() {
   return (
