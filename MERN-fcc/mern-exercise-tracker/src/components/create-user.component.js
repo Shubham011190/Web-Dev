dfa8abb840b73070a03228fa2077d4c1aa9a421d
Shubham Singhal <shubham.011190@gmail.com>
2020-04-16 19:38:58 +0530

    Create-User

--- MERN-fcc/mern-exercise-tracker/src/components/create-user.component.js
+++ MERN-fcc/mern-exercise-tracker/src/components/create-user.component.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState} from 'react';
 
-function CreateUser() {
+function CreateUser(props) {
+
+    const [username, setUsername] = useState("");
+    const onChangeUsername= event => {
+        setUsername(event.target.value)
+    };
+
+    const onSubmit = event => {
+        event.preventDefault();
+        const user = {
+            username:{username}
+        }
+        console.log(user);
+        //window.location = '/';        We want to keep the user on the same page.
+        setUsername('');
+    }
+
+
+
+
     return(
-        <div>
-            <p>You are in Create User Component</p>
+        <div className="container">
+            <h3>Create New User</h3>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label>Username: </label>
+                    <input type='text' required className='form-control' value={username} onChange={onChangeUsername} />                    
+                </div>
+                <div className="form-group">
+                    <input type='submit' value="Create User" className="btn btn-primary" />
+                </div>
+            </form>
         </div>
     )
 }
