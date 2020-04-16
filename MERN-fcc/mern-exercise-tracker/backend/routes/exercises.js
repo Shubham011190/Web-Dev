1c8a5ea680d93f27e3304249df5fa76539f4c858
Shubham Singhal <shubham.011190@gmail.com>
2020-04-16 13:39:52 +0530

    Frontend

--- MERN-fcc/mern-exercise-tracker/backend/routes/exercises.js
+++ MERN-fcc/mern-exercise-tracker/backend/routes/exercises.js
@@ -29,6 +29,27 @@ router.route('/:id').get((req, res) => {
         .catch(err => res.status(400).json("Err = " + err));
 });
 
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+        .then(() => res.json("Exercise deleted successfully"))
+        .catch(err => res.status(400).json("Err : " + err));
+})
+
+router.route('/update/:id').put((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => {
+            exercise.username = req.body.username;
+            exercise.description = req.body.description;
+            exercise.duration = Number(req.body.duration);
+            exercise.date = Date.parse(req.body.date);
+
+            exercise.save()
+                .then(() => res.json("Exercise Updated!"))
+                .catch(err => res.status(400).json("Err : " + err));
+        })
+        .catch(err => res.status(400).json("Err : " + err));
+});
+
  
 
 module.exports = router;
