b53e9485f19b3a740b4ed3ab301338786c57a68c
Shubham Singhal <shubham.011190@gmail.com>
2020-04-16 16:17:03 +0530

    Update

--- MERN-fcc/mern-exercise-tracker/src/components/create-exercise.component.js
+++ MERN-fcc/mern-exercise-tracker/src/components/create-exercise.component.js
@@ -1,6 +1,46 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 
-function CreateExercise() {
+function CreateExercise(props) {
+    const [username, setUsername] = useState("");
+    const [description, setDescription] = useState("");
+    const [duration, setDuration] = useState(0);
+    const [date, setDate] = useState(new Date());
+    const [users, setUsers] = useState([]);
+
+    const onChangeUsername = event => {
+        setUsername(event.target.value);
+    };
+      const onChangeDescription = event => {
+        setDescription(event.target.value);
+    };
+      const onChangeDuration = event => {
+        setDuration(event.target.value);
+    };
+      const onChangeDate = date => {
+        setDate(date);
+    };
+    
+    const onSubmit = event => {
+        event.preventDefault();
+
+        const exercise = {
+            username: { username },
+            description: { description },
+            duration: { duration },
+            date: { date }
+        };
+        console.log(exercise);
+        window.location = '/';
+    }
+    
+    useEffect(() => {
+        setUsers(['test user']);
+        setUsername('testUser');   
+    })
+
+
+    
+
     return(
         <div>
             <p>You are in Create Exercise Component</p>
