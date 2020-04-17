54caf31bd6523a7a1c9091fdf1b06bbc65a6efa5
Shubham Singhal <shubham.011190@gmail.com>
2020-04-17 21:26:48 +0530

    index.js

--- Chat-App/client/src/App.js
+++ Chat-App/client/src/App.js
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Join from './components/Join.js'
 import Chat from './components/Chat'
 
-const App = () => {
+const App = () => (
     <Router>
         <Route path='/' exact component={Join} />
-        <Route path='/' component={Chat} />
+        <Route path='/chat' component={Chat} />
     </Router>
-};
+);
 
 export default App;
