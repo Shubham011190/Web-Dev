6d7733e466427ae19d85e79d5d57bb5927b38cd9
Shubham Singhal <shubham.011190@gmail.com>
2020-04-17 21:22:15 +0530

    Components created

---
+++ Chat-App/client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Join from './components/Join.js'
+import Chat from './components/Chat'
+
+const App = () => {
+    <Router>
+        <Route path='/' exact component={Join} />
+        <Route path='/' component={Chat} />
+    </Router>
+};
+
+export default App;
