010217721987fd2909d1fb245a6b896ec4a36c3a
Shubham Singhal <shubham.011190@gmail.com>
2020-04-18 11:30:38 +0530

    Message transfer

--- Chat-App/server/index.js
+++ Chat-App/server/index.js
@@ -15,9 +15,19 @@ io.on('connection', (socket) => {
     socket.on('Join', ({name,room},callback) => {
         const { error, user } = addUser({ id:socket.id, name, room });
         if (error) return callback(error);
-        socket.join(user.room);
 
+        socket.emit('message', { user: "admin", text: '' + user.name + ', welcome to the room : ' + user.room });
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: '' + user.name + " has joined!" });
+        socket.join(user.room);
+        callback();
     })
+
+    socket.on('sendMessage', ({ message, callback }) => {
+        const user = getUser(socket.id);
+        io.to(user.room).emit('message', { user: user.name, message: message });
+        callback();
+    });
+
     socket.on('disconnect', () => {
         console.log('User has left!');
     })
