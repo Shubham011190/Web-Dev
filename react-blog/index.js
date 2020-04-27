16874dce15f5430e7b5eff0a69279b34633547c3
Shubham Singhal <shubham.011190@gmail.com>
2020-04-28 00:21:40 +0530

    DB connected

---
+++ react-blog/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://Shubham011190:Imshubham1619@react-blog-ahaea.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Successfully connected to DB"))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send("Hello!");
+});
+
+app.listen(3000, () => {
+    console.log("Server started at port 3000");
+});
