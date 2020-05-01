52e10816320406ec4cbecbb4ab34f08efa13b120
Shubham Singhal <shubham.011190@gmail.com>
2020-05-02 00:07:19 +0530

    UsersList created

--- mern-places-tutorial/src/user/pages/Users.js
+++ mern-places-tutorial/src/user/pages/Users.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
+    const USERS = [{
+        id: 1,
+        name: "Shubham",
+        image: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+        places: 3
+    }];
     return (
-        <h2>Users Works!</h2>
+        <UsersList items={USERS}/>
     )
 }
 
