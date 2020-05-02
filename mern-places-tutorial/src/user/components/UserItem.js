eb56635923e23d4ab08bf6bfd4ec5d290bffadce
Shubham Singhal <shubham.011190@gmail.com>
2020-05-02 10:08:25 +0530

    UserItem updated

--- mern-places-tutorial/src/user/components/UserItem.js
+++ mern-places-tutorial/src/user/components/UserItem.js
@@ -3,7 +3,17 @@ import './UserItem.css';
 
 const UserItem = props => {
     return (
-        
+        <li className='users-item'>
+            <div className='user-item__content'>
+                <div className='user-item__image'>
+                    <img src={props.img} alt={props.name} />
+                </div>
+                <div className='user-item__info'>
+                    <h2>{props.name}</h2>
+                    <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
+                </div>
+            </div>
+        </li>
     )
 }
 
