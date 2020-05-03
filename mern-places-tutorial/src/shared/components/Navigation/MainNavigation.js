66b5fb987ee42638a029a9f3ba9dd7f81cde4820
Shubham Singhal <shubham.011190@gmail.com>
2020-05-03 21:52:54 +0530

    Navigation added in /shared

---
+++ mern-places-tutorial/src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './MainNavigation.css';
+import MainHeader from './MainHeader';
+
+const MainNavigation = props => {
+    return (
+        <MainHeader>
+
+        </MainHeader>
+    )
+}
+
+export default MainNavigation;
