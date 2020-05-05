f76cac440ebaef3b56fd532571d8af7d61b28b51
Shubham Singhal <shubham.011190@gmail.com>
2020-05-05 13:43:42 +0530

    Update

--- mern-places-tutorial/src/shared/components/Navigation/MainNavigation.js
+++ mern-places-tutorial/src/shared/components/Navigation/MainNavigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './MainNavigation.css';
 import MainHeader from './MainHeader';
@@ -6,7 +7,17 @@ import MainHeader from './MainHeader';
 const MainNavigation = props => {
     return (
         <MainHeader>
-
+            <button className='main-navigation__menu-btn'>
+                <span />
+                <span />
+                <span />
+            </button>
+            <h1 className='main-navigation__tile'>
+               <Link to='/'>YourPlaces</Link>
+            </h1>
+            <nav>
+                ...
+            </nav>
         </MainHeader>
     )
 }
